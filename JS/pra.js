c1204d30eba122b905408582f305625055316f29
Shruti Singh <shru9113@gmail.com>
2025-09-15 21:32:43 +0530

    commit

--- JS/pra.js
+++ JS/pra.js
@@ -130,3 +130,37 @@
 // getData();
 // displayData();
 // console.log("other Application")
+
+
+
+
+
+function register(){
+    console.log("register end");
+    waitfortwoseconds()
+}
+function sendemail(){
+    console.log("email end");
+    waitfortwoseconds()
+}
+function login(){
+    console.log(" login end");
+    waitfortwoseconds()
+}
+function getData(){
+    console.log("data end");
+    waitfortwoseconds()
+}   
+function displayData(){
+    console.log("Display end");
+}
+function waitfortwoseconds(){
+    const ms=new Date().getTime()+2000;
+    while(new Date().getTime()<ms);
+}
+register();
+sendemail();
+login();
+getData();
+displayData();
+console.log("other Application")
